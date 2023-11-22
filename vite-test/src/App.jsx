ae24cedec0345ff4af1d63bcd5bd5bb1c669c5a8
Jyoti <jyotiraghav95@gmail.com>
2023-11-22 17:02:05 +0530

    recent changes

---
+++ vite-test/src/App.jsx
@@ -0,0 +1,10 @@
+import Form from "./components/Form";
+import "./index.css";
+const App = () => {
+  return (
+    <main className="main">
+      <Form />
+    </main>
+  );
+};
+export default App;
