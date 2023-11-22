7d2589c96144d678b32ff510750dfe25f601a9ac
Jyoti <jyotiraghav95@gmail.com>
2023-11-22 17:15:22 +0530

    recent changes

--- src/assets/icons/index.js
+++
@@ -1,8 +0,0 @@
-import advanced from "./icon-advanced.svg"
-import arcade from "./icon-arcade.svg"
-import checkMark from "./icon-checkmark.svg"
-import pro from "./icon-pro.svg"
-import thankYou from "./icon-thank-you.svg"
-
-export {advanced, arcade, checkMark, pro, thankYou}
-
--- src/assets/images/index.js
+++
@@ -1,4 +0,0 @@
-import mobileSideBar from "./bg-sidebar-mobile.svg"
-import desktopSideBar from "./bg-sidebar-desktop.svg"
-
-export {mobileSideBar, desktopSideBar}
